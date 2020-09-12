35727e355085eb58d567555bdf4a564262e81edc
shinichi <0503baku@gmail.com>
2020-09-13 01:19:22 +0900

    ブログアプリ課題提出

---
+++ tweet/show.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once('config.php');
+require_once('functions.php');
+
+$id = $_GET['id'];
+
+$dbh = connectDb();
+$sql = 'SELECT * FROM tweets WHERE id = :id ';
+$stmt = $dbh->prepare($sql);
+$stmt->bindParam(':id', $id, PDO::PARAM_INT);
+$stmt->execute();
+
+$tweet = $stmt->fetch(PDO::FETCH_ASSOC);
+
+// 存在しないidを渡された場合はindex.phpへ飛ばす
+if (!$tweet) {
+    header('Location: index.php');
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Tweet詳細</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1>テストツイート</h1>
+    <a href="index.php">戻る</a>
+    <ul class="tweet-list">
+        <li>
+            [#<?= h($tweet['id']) ?>]
+            <?= h($tweet['content']) ?><br>
+            投稿日時: <?= h($tweet['created_at']) ?>
+
+            <?php if ($tweet['good']) : ?>
+                <a href="good.php?id=<?= ($tweet['id']) ?>" class="good-link">★</a>
+
+            <?php else : ?>
+                <a href="good.php?id=<?= ($tweet['id']) ?>" class="bad-link">☆</a>
+
+            <?php endif; ?>
+            <a href="edit.php?id=<?= h($tweet['id']) ?>">[編集]</a>
+            <a href="delete.php?id=<?= h($tweet['id']) ?>">[削除]</a>
+            <hr>
+        </li>
+    </ul>
+</body>
+
+</html>
